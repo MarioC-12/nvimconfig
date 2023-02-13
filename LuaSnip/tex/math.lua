0a0e407738a7aed7524c5e54659011cbf4eda7f9
Mario <mariocm2002@hotmail.com>
2023-02-13 09:27:09 +0100

    Created sombreros.lua

--- LuaSnip/tex/math.lua
+++ LuaSnip/tex/math.lua
@@ -261,61 +261,6 @@ return
         ),
         {condition = in_mathzone}
     ),
-    s({trig="bar", snippetType="autosnippet", dscr="Bar", priority=10},
-        fmta(
-            [[ 
-            \overline{<>}
-            ]],
-            {
-                d(1, get_visual),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="tilde", snippetType="autosnippet", dscr="Tilde", priority=10},
-        fmta(
-            [[ 
-            \tilde{<>}
-            ]],
-            {
-                d(1, get_visual),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="vet", snippetType="autosnippet", dscr="Vector", priority=10},
-        fmta(
-            [[ 
-            \overrightarrow{<>}
-            ]],
-            {
-                d(1, get_visual),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="hat", snippetType="autosnippet", dscr="Hat", priority=10},
-        fmta(
-            [[ 
-            \hat{<>}
-            ]],
-            {
-                d(1, get_visual),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="ino", snippetType="autosnippet", dscr="[In]teri[o]r", priority=10},
-        fmta(
-            [[ 
-            \mathring{<>}
-            ]],
-            {
-                d(1, get_visual),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
     s({trig="//", snippetType="autosnippet", dscr="Fraction"},
         fmta(
             [[ 
@@ -360,61 +305,6 @@ return
             {
                 f( function(_, snip) return snip.captures[1] end ),
                 f( function(_, snip) return snip.captures[2] end ),
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="(%w+)hat", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP sombrero"},
-        fmta(
-            [[ 
-            \hat{<>}
-            ]],
-            {
-                f( function(_, snip) return snip.captures[1] end )
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="(%w+)tilde", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP tilde"},
-        fmta(
-            [[ 
-            \tilde{<>}
-            ]],
-            {
-                f( function(_, snip) return snip.captures[1] end )
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="(%w+)bar", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP bar"},
-        fmta(
-            [[ 
-            \overline{<>}
-            ]],
-            {
-                f( function(_, snip) return snip.captures[1] end )
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="(%w+)vet", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP vet"},
-        fmta(
-            [[ 
-            \overrightarrow{<>}
-            ]],
-            {
-                f( function(_, snip) return snip.captures[1] end )
-            }
-        ),
-        {condition = in_mathzone}
-    ),
-    s({trig="(%w)ino", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP [in]teri[o]r"},
-        fmta(
-            [[ 
-            \mathring{<>}
-            ]],
-            {
-                f( function(_, snip) return snip.captures[1] end )
             }
         ),
         {condition = in_mathzone}
---
+++ LuaSnip/tex/sombreros.lua
@@ -0,0 +1,119 @@
+local helpers = require('luasnip-helper-funcs')
+local get_visual = helpers.get_visual
+local in_mathzone = function()
+    return vim.fn['vimtex#syntax#in_mathzone']() == 1
+end
+
+return
+{
+    s({trig="bar", snippetType="autosnippet", dscr="Bar", priority=10},
+        fmta(
+            [[ 
+            \overline{<>}
+            ]],
+            {
+                d(1, get_visual),
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="tilde", snippetType="autosnippet", dscr="Tilde", priority=10},
+        fmta(
+            [[ 
+            \tilde{<>}
+            ]],
+            {
+                d(1, get_visual),
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="vet", snippetType="autosnippet", dscr="Vector", priority=10},
+        fmta(
+            [[ 
+            \overrightarrow{<>}
+            ]],
+            {
+                d(1, get_visual),
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="hat", snippetType="autosnippet", dscr="Hat", priority=10},
+        fmta(
+            [[ 
+            \hat{<>}
+            ]],
+            {
+                d(1, get_visual),
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="ino", snippetType="autosnippet", dscr="[In]teri[o]r", priority=10},
+        fmta(
+            [[ 
+            \mathring{<>}
+            ]],
+            {
+                d(1, get_visual),
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="(%w+)hat", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP sombrero"},
+        fmta(
+            [[ 
+            \hat{<>}
+            ]],
+            {
+                f( function(_, snip) return snip.captures[1] end )
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="(%w+)tilde", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP tilde"},
+        fmta(
+            [[ 
+            \tilde{<>}
+            ]],
+            {
+                f( function(_, snip) return snip.captures[1] end )
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="(%w+)bar", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP bar"},
+        fmta(
+            [[ 
+            \overline{<>}
+            ]],
+            {
+                f( function(_, snip) return snip.captures[1] end )
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="(%w+)vet", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP vet"},
+        fmta(
+            [[ 
+            \overrightarrow{<>}
+            ]],
+            {
+                f( function(_, snip) return snip.captures[1] end )
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+    s({trig="(%w)ino", wordTrig=false, regTrig = true, snippetType="autosnippet", dscr="OP [in]teri[o]r"},
+        fmta(
+            [[ 
+            \mathring{<>}
+            ]],
+            {
+                f( function(_, snip) return snip.captures[1] end )
+            }
+        ),
+        {condition = in_mathzone}
+    ),
+}
