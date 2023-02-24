4ab1545261e096b1380a2e21e5d77f4570c639d7
Mario <mcalvarr@ucm.es>
2023-02-24 20:06:03 +0100

    Fix tab

--- init.lua
+++ init.lua
@@ -501,8 +501,6 @@ cmp.setup {
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -510,8 +508,6 @@ cmp.setup {
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
