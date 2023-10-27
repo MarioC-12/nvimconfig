80cf208731b970acef7dcb4e2da6dab1eaf69d12
Mario <mariocm2002@hotmail.com>
2023-10-27 22:36:00 +0200

    Fix rust_tools

--- after/plugin/rust_tools.lua
+++ after/plugin/rust_tools.lua
@@ -1,22 +1,40 @@
--- Update this path
-local mason_registry = require('mason-registry')
---local codelldb = mason_registry.get_package('codelldb')
---local extension_path = codelldb:get_install_path()
---local codelldb_path = extension_path .. '/codelldb'
---local liblldb_path = extension_path .. '/extension/lldb/lib/liblldb.so'
-
 local rt = require("rust-tools")
 
-rt.setup({
+local function on_attach(client, buffer)
+  -- This callback is called when the LSP is atttached/enabled for this buffer
+  -- we could set keymaps related to LSP, etc here.
+end
+
+local opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
   server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  }
-  --dap = {
-  --    adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
-  --}
-})
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      -- You must install it on your own
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+rt.setup(opts)
