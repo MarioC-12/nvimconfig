0715bea47dd8af30456b7627281d6d75f85d65de
Mario <mariocm2002@hotmail.com>
2023-11-03 11:58:09 +0100

    Changed map

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -30,7 +30,7 @@ vim.keymap.set('n', 'N', "Nzzzv")
 
 -- Move between windows
 vim.keymap.set('n', '<C-j>', "<C-W>j", { })
-vim.keymap.set('n', '<C-k>', "<C-W>k", { })
+vim.keymap.set('n', '<leader><C-k>', "<C-W>k", { })
 vim.keymap.set('n', '<C-h>', "<C-W>h", { })
 vim.keymap.set('n', '<C-l>', "<C-W>l", { })
 
