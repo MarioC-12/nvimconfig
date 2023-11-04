71039d2dbccaa19db48ebf9b021dbbb37414c5d3
mario <mariocm2002@hotmail.com>
2023-11-04 17:17:49 +0100

    Improved performance of luasnips

--- lua/plugins/luasnips.lua
+++ lua/plugins/luasnips.lua
@@ -16,7 +16,7 @@ local luasnip_opts = function ()
         store_selection_keys = "<Tab>",
     })
 
-    require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+    require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
 end
 
 return {
