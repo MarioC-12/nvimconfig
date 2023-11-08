4cef57c176d53b951596ef3bc03f07af9455cb47
Mario <mariocm2002@hotmail.com>
2023-11-08 12:59:35 +0100

    Haskell lsp

---
+++ lua/plugins/haskell.lua
@@ -0,0 +1,24 @@
+local helpers = require("luasnip-helper-funcs")
+
+local config_opts = function ()
+    local ht = require('haskell-tools')
+    vim.g.haskell_tools = {
+        hls = {
+            on_attach = helpers.on_attach,
+        },
+        repl = {
+            handler = 'toggleterm',
+        },
+    }
+    vim.keymap.set('n', '<leader>rr', ht.repl.toggle)
+    vim.keymap.set('n', '<leader>rf', function()
+        ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+    end)
+end
+
+return {
+    'mrcjkb/haskell-tools.nvim',
+    version = '^3',
+    config = config_opts,
+    ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+}
