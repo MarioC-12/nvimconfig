616635c2195dec4110322df8a8bbddee026b711f
Mario <mariocm2002@hotmail.com>
2023-12-15 14:55:40 +0100

    Changed install

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 
 # Rename 
 if [ -d "$HOME/.config/nvim/" ]; then 
-    backup_folder_name="$HOME/.config/nvim_backup"
+    backup_folder_name="$HOME/.config/nvim_bak"
     echo "There is a previous configuration. Making a backup in $backup_folder_name."
-    mv "$HOME/.config/nvim/" "$backup_folder_name"
+    mv "$HOME/.config/nvim" "$backup_folder_name"
 fi
 
-# Symbolic link
-ln -s $(pwd) "$HOME/.config/nvim"
-echo "The configuration has been successfully installed. Please open neovim to 
-install the plugins."
+mkdir "$HOME/.config/nvim" 2> /dev/null     # Silent error output
+cp -r . "$HOME/.config/nvim"
+echo "The configuration has been successfully installed. Please open neovim to install the plugins."
