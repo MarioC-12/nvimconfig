0986c04756593fc074c4436c884ac197d248bfff
Mario <mariocm2002@hotmail.com>
2023-12-15 10:52:36 +0100

    Changed cmp.confirm mapping

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,7 +13,7 @@ local cmp_opts = function()
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
-            ['<CR>'] = cmp.mapping.confirm {
+            ['<C-y>'] = cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
             },
